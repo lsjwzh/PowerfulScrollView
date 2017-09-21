f5d2a20487df96bb2e7fe977018d78edf4951407
lsjwzh <lsjwzh@gmail.com>
2017-09-21 18:00:25 +0800

    Update Demo

---
+++ app/src/main/java/com/support/android/designlibdemo/EmbedRecyclerViewDemoActivity.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.support.android.designlibdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.lsjwzh.widget.multirvcontainer.MultiRVScrollView;
+
+public class EmbedRecyclerViewDemoActivity extends AppCompatActivity {
+  MultiRVScrollView mMultiRVScrollView;
+  RecyclerView mRecyclerView;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.embed_rv_demo);
+    loadBackdrop();
+    mMultiRVScrollView = (MultiRVScrollView) findViewById(R.id.main_content);
+    mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+    mRecyclerView.setLayoutManager(new LinearLayoutManager(mRecyclerView.getContext()));
+    mRecyclerView.setAdapter(new SimpleStringRecyclerViewAdapter(this,
+        DemoUtils.getRandomSublist(Cheeses.sCheeseStrings, 200)));
+    mMultiRVScrollView.takeOverScrollBehavior(mRecyclerView);
+  }
+
+  private void loadBackdrop() {
+    final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
+    Glide.with(this).load(Cheeses.getRandomCheeseDrawable()).centerCrop().into(imageView);
+    imageView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        mRecyclerView.scrollToPosition(10);
+      }
+    });
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.sample_actions, menu);
+    return true;
+  }
+}
