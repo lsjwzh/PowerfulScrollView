b207150e6878c26b00a3cd8aab77297cb16ac044
lsjwzh <lsjwzh@gmail.com>
2017-09-25 18:55:06 +0800

    try to fix some layout bug

--- widget.multirvcontainer/src/main/java/com/lsjwzh/widget/multirvcontainer/NestRecyclerViewHelper.java
+++ widget.multirvcontainer/src/main/java/com/lsjwzh/widget/multirvcontainer/NestRecyclerViewHelper.java
@@ -19,6 +19,8 @@ class NestRecyclerViewHelper {
                                    int oldTop, int oldRight, int oldBottom) {
           if (mNestedRecyclerView.getHeight() > mHostScrollView.getHeight()) {
             mNestedRecyclerView.getLayoutParams().height = mHostScrollView.getHeight();
+            // sometimes requestLayout will not cause a layout action, so call forceLayout before
+            mNestedRecyclerView.forceLayout();
             mNestedRecyclerView.requestLayout();
           }
         }
