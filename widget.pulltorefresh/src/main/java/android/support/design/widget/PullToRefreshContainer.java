2f1c1b9b0ca079b47f08856694939f15b9621a1f
lsjwzh <lsjwzh@gmail.com>
2018-07-02 14:22:30 +0800

    删除多余代码

--- widget.pulltorefresh/src/main/java/android/support/design/widget/PullToRefreshContainer.java
+++ widget.pulltorefresh/src/main/java/android/support/design/widget/PullToRefreshContainer.java
@@ -149,7 +149,6 @@ public class PullToRefreshContainer extends MultiRVScrollView {
             refreshChild.getRefreshHeader().cancelAnimation();
             refreshChild.getRefreshHeader().setVisibleHeight((int) translationY);
             refreshChild.getRefreshTargetView().setTranslationY(translationY);
-            return;
           }
         }
       }
